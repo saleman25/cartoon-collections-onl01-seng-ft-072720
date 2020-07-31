fac9d4bfcc53dd004362d48d87e0a0575f62ff62
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-31 01:03:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name)
+  i = 0 
+  while i < name.length 
+  puts "1. #{name} 2. #{name} 3. #{name} 4. #{name} 5. #{name} 6. #{name} 7. #{name}"
+  i = i + 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(food)
+ food.map do |i|
+   i.capitalize + "!"
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+   calls.any? do |i|
+    i.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  index = 0
+  array.find do |i|
+    i == cheese_types[index]
+  end
 end
+
